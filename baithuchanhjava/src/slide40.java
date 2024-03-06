14ce7f8846d6c18686a7f8502bbe42af2700b17c
ttlinh2004 <trantronglinh2004@gmail.com>
2024-03-06 22:26:02 +0700

    slide40

---
+++ baithuchanhjava/src/slide40.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class slide40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap ngay: ");
+        int ngay = sc.nextInt();
+
+        switch (ngay) {
+            case 1:
+                System.out.println("Chu nhat");
+                break;
+            case 2:
+                System.out.println("Thu hai");
+                break;
+            case 3:
+                System.out.println("Thu ba");
+                break;
+            case 4:
+                System.out.println("Thu tu");
+                break;
+            case 5:
+                System.out.println("Thu nam");
+                break;
+            case 6:
+                System.out.println("Thu sau");
+                break;
+            case 7:
+                System.out.println("Thu bay");
+            default:
+                System.out.println("So ngay khong hop le.");
+                break;
+        }
+        
+        sc.close();
+    }
+}
