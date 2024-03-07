85e65f28075353ef9ad26889ef3d688da1077310
ttlinh2004 <trantronglinh2004@gmail.com>
2024-03-07 08:58:20 +0700

    bai7

---
+++ lab2/src/bai7.java
@@ -0,0 +1,9 @@
+public class bai7 {
+    public static void main(String[] args) {
+        System.out.println("20 so nguyen duong dau tien la: ");
+        
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
